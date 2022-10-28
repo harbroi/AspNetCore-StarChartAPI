7a75f6e76bafd191363685289a38b9c92bf5f10a
harbroi <haroldinacay@gmail.com>
2022-10-28 19:51:50 +0800

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -17,5 +17,41 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
+        [HttpGet("{id:int}", Name = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestial = _context.CelestialObjects.Where(x => x.Id == id);
+            if(celestial == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var satellites = _context.CelestialObjects.Where(x => x.Id == id).ToList();
+                return Ok(satellites);
+            }
+        }
+
+        [HttpGet("{name}", Name = "GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var celestial = _context.CelestialObjects.Where(x => x.Name == name);
+            if (celestial == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var satellites = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+                return Ok(celestial);
+            }
+        }
+
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            var celestial = _context.CelestialObjects.ToList();
+            return Ok(celestial);
+        }
     }
 }
